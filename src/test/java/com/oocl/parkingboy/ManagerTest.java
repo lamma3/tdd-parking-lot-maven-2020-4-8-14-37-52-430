e1dfe6232541f96754f170653d39a1ef0a9fbe95
Marco Lam <lamma3@oocl.com>
2020-04-09 22:35:49 +0800

    [Marco] add test case should_able_to_specify_parking_boy_to_park

--- src/test/java/com/oocl/parkingboy/ManagerTest.java
+++ src/test/java/com/oocl/parkingboy/ManagerTest.java
@@ -1,4 +1,22 @@
 package com.oocl.parkingboy;
 
+import com.oocl.Car;
+import com.oocl.ParkingLot;
+import com.oocl.ParkingTicket;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ManagerTest {
+
+    @Test
+    public void should_able_to_specify_parking_boy_to_park() {
+        ParkingLot parkingLot = new ParkingLot();
+        Manager manager = new Manager(parkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        manager.add(parkingBoy);
+
+        Car car = new Car();
+        ParkingTicket parkingTicket = manager.park(car, parkingBoy);
+        Assertions.assertNotNull(parkingTicket);
+    }
 }
