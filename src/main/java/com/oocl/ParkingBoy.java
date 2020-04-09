3e8367124c19a7f6af70a7cae3e5bd5329f2b9ba
Marco Lam <lamma3@oocl.com>
2020-04-09 15:05:46 +0800

    [Marco] add test case should_return_car_when_given_ticket

--- src/main/java/com/oocl/ParkingBoy.java
+++ src/main/java/com/oocl/ParkingBoy.java
@@ -18,4 +18,8 @@ public class ParkingBoy {
         parkingTicketList.add(parkingTicket);
         return parkingTicket;
     }
+
+    public Car fetch(ParkingTicket parkingTicket) {
+        return null;
+    }
 }
--- src/test/java/com/oocl/ParkingBoyTest.java
+++ src/test/java/com/oocl/ParkingBoyTest.java
@@ -13,4 +13,14 @@ public class ParkingBoyTest {
         ParkingTicket parkingTicket = packingBoy.park(car);
         Assert.assertEquals(car, parkingTicket.getCar());
     }
+
+    @Test
+    public void should_return_car_when_given_ticket() {
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy packingBoy = new ParkingBoy(parkingLot);
+        Car car = new Car();
+        ParkingTicket parkingTicket = packingBoy.park(car);
+        Car result = packingBoy.fetch(parkingTicket);
+        Assert.assertEquals(car, result);
+    }
 }
